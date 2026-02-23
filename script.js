9987e796e135889a7cb63578eea60ebdc6be77be
FrGp-dev <fredy.alexander.gonzalez.pobre@gmail.com>
2026-02-23 18:50:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     if (downloadBtn) {
         downloadBtn.addEventListener('click', () => {
-            // Ejecuta el comando de impresión del navegador
-            // Gracias al CSS @media print, se verá como un CV profesional
-            window.print();
+            const pdfUrl = 'https://raw.githubusercontent.com/FrGp-dev/Hoja-de-Vida/5adf91437f10774ea64e2c190fd132c86512c0e6/Pdf.pdf';
+            const link = document.createElement('a');
+            link.href = pdfUrl;
+            link.download = 'Hoja_de_Vida.pdf';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
         });
     }
 
-    console.log("Sistema de descarga listo.");
+    console.log("Sistema de descarga de archivo PDF listo.");
 });
